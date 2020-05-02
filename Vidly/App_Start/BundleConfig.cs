c73b7f7bb0fc5c2d2abb89a72982165e48199c16
Muhammad Aamir <51251914+MuhammadAamir21@users.noreply.github.com>
2020-05-02 17:03:52 +0500

    Bootbox Plug-in

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace Vidly
 
             //This is bootsrap bunddle
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js"));
 
             //This is for css assests bunddle
             bundles.Add(new StyleBundle("~/Content/css").Include(
