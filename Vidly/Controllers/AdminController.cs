8c4084386cdd237cc769146d917a26eec8ab270b
Muhammad Aamir <51251914+MuhammadAamir21@users.noreply.github.com>
2020-05-05 16:59:42 +0500

    View All Users

---
+++ Vidly/Controllers/AdminController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public AdminController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Admin
+        public ActionResult Users()
+        {
+            var usersWithRoles = (from user in _context.Users
+                                  select new
+                                  {
+                                      UserId = user.Id,
+                                      Username = user.UserName,
+                                      Email = user.Email,
+                                      RoleNames = (from userRole in user.Roles
+                                                   join role in _context.Roles on userRole.RoleId
+                                                   equals role.Id
+                                                   select role.Name).ToList()
+                                  }).ToList().Select(p => new UsersViewModel()
+
+                                  {
+                                      UserId = p.UserId,
+                                      Username = p.Username,
+                                      Email = p.Email,
+                                      Role = string.Join(",", p.RoleNames).Replace(";", "")
+                                  });
+
+            return View(usersWithRoles);
+        }
+
+        public ActionResult Indesx()
+        {
+            _context.Users.Include(u => u.Roles).ToList();
+
+            var viewModel = new AdminViewModel
+            {
+                Roles = _context.Roles.ToList(),
+            };
+
+            return View();
+        }
+    }
+}
---
+++ Vidly/ViewModels/AdminViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace Vidly.ViewModels
+{
+    public class AdminViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string Name { get; set; }
+
+        public IEnumerable<IdentityRole> Roles { get; set; }
+    }
+}
